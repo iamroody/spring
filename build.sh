6a5aea957be7fb7a2f735161a2fc4b704adddb79
roody <wangyan87@gmail.com>
2012-01-07 00:12:17 +0800

    add bundler

--- build.sh
+++ build.sh
@@ -15,7 +15,6 @@ function check_rvm {
 	load_rvm
 }
 
-
 function install_ruby {
 	rvm install 1.8.7-p352
 	rvm use 1.8.7-p352
@@ -28,10 +27,21 @@ function check_ruby {
 	rvm use 1.8.7-p352@spring
 }
 
+function install_bundler {
+  gem sources | grep "http://rubygems.org/" > /dev/null || gem sources -a http://rubygems.org/ && \
+  gem sources | grep "http://gems.rubyforge.org/" > /dev/null || gem sources -a http://gems.rubyforge.org/ && \
+  gem install bundler --no-ri --no-rdoc
+}
+
+function check_bundler {
+	which bundle | grep 1.8.7-p352 > /dev/null || install_bundler
+}
+
 function main {
 	case $1 in
 		check_rvm) check_rvm;;
 		check_ruby) check_ruby;;
+		check_bundler) check_bundler;;
 		*) exit 1;;
 	esac
 }
